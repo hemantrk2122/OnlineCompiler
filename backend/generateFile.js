b630fad4a25fa84707e020a4da7c8c0c79b3f635
hemantrk2122 <hemantrk2122@gmail.com>
2023-07-08 22:11:26 +0530

    Added important comments

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -1,6 +1,11 @@
+// The FS(File System) module is used for working with the the file system in Node.js. It can read, write, and modify file data.
 const fs = require('fs');
+// The Path module provides methods for working with file and directory paths. 
+// It can join paths, get the current working directory and resolve relative paths
 const path = require('path');
+// The UUID (Unique Universal Identifier) module is used for generating random unique IDs.
 const { v4: uuid } = require('uuid');
+
 
 const dirCodes = path.join(__dirname, 'codes');
 if (!fs.existsSync(dirCodes)) {
